b3469e6a67e3f4f5de6bdcad92ae885d84daaa08
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-09-29 15:47:09 +0000

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import favicon from './favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href={favicon.src} />
       <body className={inter.className}>{children}</body>
     </html>
   )
